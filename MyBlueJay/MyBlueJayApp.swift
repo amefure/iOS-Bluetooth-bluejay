66e37a2b43990324f467178f934b625a345dced9
amefure <shachi10969@gmail.com>
2023-11-16 22:42:22 +0900

    Initial Commit

---
+++ MyBlueJay/MyBlueJayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBlueJayApp.swift
+//  MyBlueJay
+//
+//  Created by t&a on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct MyBlueJayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
